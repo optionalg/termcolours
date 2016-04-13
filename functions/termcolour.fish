59c19658e3ed18197cce7b66efa59ec9c035dae9
Marcel Bischoff <marcel@herrbischoff.com>
2016-04-13 20:20:47 +0200

    Inital commit

---
+++ functions/termcolour.fish
@@ -0,0 +1,7 @@
+function termcolour
+    set herrbischoff_tcolors (echo $TERM | grep -oE '2|4|8|16|32|64|128|256')
+    for i in (seq 0 $herrbischoff_tcolors)
+        printf "\x1b[38;5;%smcolour%s\n" $i $i
+    end
+    unset $herrbischoff_tcolors
+end
